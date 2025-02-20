529fbb178f4dd928b385dc7bcab295a10f46e2f4
idir <idirkeddache@gmail.com>
2025-02-20 09:05:37 +0100

    admin navbare

--- src/app/slices/user.js
+++ src/app/slices/user.js
@@ -1,20 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
+import admin from "./admin";
 
 export const userSlice = createSlice({
   name: "user",
   initialState: {
     user: null,
     isLoggedIn: false,
+    isAdmin: false,
   },
+
   reducers: {
     setUser: (state, action) => {
       const newUser = action.payload;
       if (newUser) {
         state.user = newUser;
         state.isLoggedIn = true;
+        state.isAdmin = newUser.role === "admin";
       } else {
         state.user = null;
         state.isLoggedIn = false;
+        state.isAdmin = false;
       }
     },
   },
